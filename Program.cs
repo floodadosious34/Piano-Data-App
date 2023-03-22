e5f61b2c0f8e741acd05fca01ea632f7b8240aed
James Flood Jr <floodadosious111@gmail.com>
2023-03-21 22:00:56 -0400

    added line

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+Console.WriteLine("Hello, Welcome tp the Piano Data App!");
+Console.WriteLine("Would you like to add a client to the list? Press \"Y\" for Yes, and \"N\" for No");
